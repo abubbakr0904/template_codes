c403fd191266300e205232a7170834315e1ac191
abubbakr0904 <abubbakroqilov03@gmail.com>
2024-05-29 10:43:35 +0300

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/template_codes/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.template_codes
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:template_codes/screens/home_screen/home_screen.dart';
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return ScreenUtilInit(
+      designSize: const Size(375, 812),
+      builder: (context, child) {
+        ScreenUtil.init(context);
+        return MaterialApp(
+          debugShowCheckedModeBanner: false,
+          home: child,
+        );
+      },
+      child: const HomeScreen(),
+    );
+  }
+}
